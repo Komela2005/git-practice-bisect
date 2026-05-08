dd346558863b84a98dcfa291a6e246ffc0f2d41f
Komela2005 <ms.tavabilova@mail.ru>
2026-05-08 23:36:44 +0300

    dev: add power and sqrt options to menu

--- person1/main.py
+++ person1/main.py
@@ -1,9 +1,10 @@
 def main():
-    print("Welcome to Calculator App")
     print("1. Addition")
     print("2. Subtraction")
     print("3. Multiplication")
     print("4. Division")
+    print("5. Power")
+    print("6. Square Root")
 
 if __name__ == "__main__":
     main()
