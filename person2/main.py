26a36147ff2a54b7f698d31de00fde45039d107d
Komela2005 <ms.tavabilova@mail.ru>
2026-05-08 20:02:47 +0300

    person2: add power function to calculator

---
+++ person2/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to Calculator App")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+if __name__ == "__main__":
+    main()
