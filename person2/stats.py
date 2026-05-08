e334243679369aaa5b4bf04d8cb34081544ae595
Komela2005 <ms.tavabilova@mail.ru>
2026-05-08 20:18:41 +0300

    dev2: add statistics module with mean function

---
+++ person2/stats.py
@@ -0,0 +1,8 @@
+# Statistics module
+def mean(numbers):
+    return sum(numbers) / len(numbers)
+def median(numbers):
+    sorted_nums = sorted(numbers)
+    n = len(sorted_nums)
+    mid = n // 2
+    return sorted_nums[mid] if n % 2 else (sorted_nums[mid-1] + sorted_nums[mid]) / 2
